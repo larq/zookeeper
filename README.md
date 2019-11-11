f8947732a88ad356db29318b3888395c67a4fe2c
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2019-11-11 14:35:58 +0000

    Update README and add example.

--- README.md
+++ README.md
@@ -8,5 +8,113 @@ A small library for configuring modular applications.
 
 ```console
 pip install zookeeper
-pip install colorama  # optional for colored console output
 ```
+
+### Components
+
+The fundamental building block of Zookeeper is a
+[`Component`](zookeeper/component.py). `Component` subclasses can have
+configurable parameters, which are declared using class-level type annotations
+(in a similar way to [Python
+dataclasses](https://docs.python.org/3/library/dataclasses.html)). These
+parameters can be Python objects or nested sub-components, and need not be set
+with a default value.
+
+For example:
+```python
+from zookeeper import Component
+
+class ChildComponent(Component):
+    a: int                  # An `int` parameter, with no default set
+    b: str = "foo"          # A `str` parameter, which by default will be `foo`
+
+class ParentComponent(Component):
+    a: int                  # The same `int` parameter as the child
+    child: ChildComponent   # A nested component parameter, of type `ChildComponent`
+```
+
+After instantiation, components can be 'configured' with a configuration
+dictionary, containing values for a tree of nested parameters. This process
+automatically injects the correct values into each parameter.
+
+If a child sub-component declares a parameter which already exists in some
+containing parent, then it will pick up the value that's set on the parent,
+unless a 'scoped' value is set on the child.
+
+For example:
+```
+p = ParentComponent()
+
+p.configure({
+    "a": 5,
+    "child.a": 4,
+})
+
+>>> 'ChildComponent' is the only concrete component class that satisfies the type
+>>> of the annotated parameter 'ParentComponent.child'. Using an instance of this
+>>> class by default.
+
+print(p)
+
+>>> ParentComponent(
+>>>     a = 5,
+>>>     child = ChildComponent(
+>>>         a = 4,
+>>>         b = "foo"
+>>>     )
+>>> )
+```
+
+### Tasks and the CLI
+
+The best way to define runnable tasks with Zookeeper is to subclass
+[`Task`](zookeeper/task.py) and override the `run` method.
+
+Zookeeper provides a small mechanism to run tasks from a CLI, using the
+decorator `@add_task_to_cli`. The CLI will automatically instantiate the task
+and call `configure()`, passing in configuration parsed from command line
+arguments.
+
+For example:
+```python
+from zookeeper import Task
+from zookeeper.cli import add_task_to_cli, cli
+
+@add_task_to_cli
+class UseChildA(Task):
+    parent: ParentComponent
+
+    def run(self):
+        print(self.parent.child.a)
+
+@add_task_to_cli
+class UseParentA(UseChildA):
+    def run(self):
+        print(self.parent.a)
+
+if __name__ == "__main__":
+    cli()
+```
+
+Running the above file then gives a nice CLI interface:
+```
+python test.py use_child_a
+>>> ValueError: No configuration value found for annotated parameter 'UseChildA.parent.a' of type 'int'.
+
+python test.py use_child_a a=5
+>>> 5
+
+python test.py use_child_a a=5 child.a=3
+>>> 3
+
+python test.py use_parent_a a=5 child.a=3
+>>> 5
+```
+
+### Using Zookeeper to define Larq or Keras experiments
+
+See [examples/larq_experiment.py](examples/larq_experiment.py) for an example of
+how to use Zookeeper to define all the necessary components (dataset,
+preprocessing, and model) of a Larq experiment: training a BinaryNet on
+CIFAR-10. This example can be easily adapted to other Larq or Keras models and
+other datasets.
---
+++ examples/larq_experiment.py
@@ -0,0 +1,167 @@
+# This is an example of how to use Zookeeper to run a Larq BinaryNet experiment
+# on CIFAR-10.
+
+import math
+from functools import partial
+from typing import Tuple, Union
+
+import larq as lq
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from zookeeper import Dataset, Experiment, Model, Preprocessing
+from zookeeper.cli import add_task_to_cli, cli
+
+
+class Cifar10(Dataset):
+    name = "cifar10"
+    # CIFAR-10 has only train and test, so validate on test.
+    train_split = tfds.Split.TRAIN
+    validation_split = tfds.Split.TEST
+
+
+class PadCropAndFlip(Preprocessing):
+    pad_size: int
+    output_size: int
+
+    def inputs(self, data, training):
+        image = data["image"]
+        if training:
+            image = tf.image.resize_with_crop_or_pad(
+                image, self.pad_size, self.pad_size
+            )
+            image = tf.image.random_crop(
+                image, (self.output_size, self.output_size, image.shape[-1])
+            )
+            image = tf.image.random_flip_left_right(image)
+        else:
+            image = tf.image.resize_with_crop_or_pad(
+                image, self.output_size, self.output_size
+            )
+        return tf.cast(image, tf.float32) / (255.0 / 2.0) - 1.0
+
+    def outputs(self, data):
+        return data["label"]
+
+
+class BinaryNet(Model):
+    dataset: Dataset
+    preprocessing: Preprocessing
+
+    filters: int = 128
+    dense_units: int = 1024
+    kernel_size: Union[int, Tuple[int, int]] = (3, 3)
+
+    def build(self, input_shape):
+        kwhparams = dict(
+            input_quantizer="ste_sign",
+            kernel_quantizer="ste_sign",
+            kernel_constraint="weight_clip",
+            use_bias=False,
+        )
+
+        return tf.keras.models.Sequential(
+            [
+                # Don't quantize inputs in first layer
+                lq.layers.QuantConv2D(
+                    self.filters,
+                    self.kernel_size,
+                    kernel_quantizer="ste_sign",
+                    kernel_constraint="weight_clip",
+                    use_bias=False,
+                    input_shape=input_shape,
+                ),
+                tf.keras.layers.BatchNormalization(scale=False),
+                lq.layers.QuantConv2D(
+                    self.filters, self.kernel_size, padding="same", **kwhparams
+                ),
+                tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+                tf.keras.layers.BatchNormalization(scale=False),
+                lq.layers.QuantConv2D(
+                    2 * self.filters, self.kernel_size, padding="same", **kwhparams
+                ),
+                tf.keras.layers.BatchNormalization(scale=False),
+                lq.layers.QuantConv2D(
+                    2 * self.filters, self.kernel_size, padding="same", **kwhparams
+                ),
+                tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+                tf.keras.layers.BatchNormalization(scale=False),
+                lq.layers.QuantConv2D(
+                    4 * self.filters, self.kernel_size, padding="same", **kwhparams
+                ),
+                tf.keras.layers.BatchNormalization(scale=False),
+                lq.layers.QuantConv2D(
+                    4 * self.filters, self.kernel_size, padding="same", **kwhparams
+                ),
+                tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+                tf.keras.layers.BatchNormalization(scale=False),
+                tf.keras.layers.Flatten(),
+                lq.layers.QuantDense(self.dense_units, **kwhparams),
+                tf.keras.layers.BatchNormalization(scale=False),
+                lq.layers.QuantDense(self.dense_units, **kwhparams),
+                tf.keras.layers.BatchNormalization(scale=False),
+                lq.layers.QuantDense(self.dataset.num_classes, **kwhparams),
+                tf.keras.layers.BatchNormalization(scale=False),
+                tf.keras.layers.Activation("softmax"),
+            ]
+        )
+
+
+@add_task_to_cli
+class BinaryNetCifar10(Experiment):
+    dataset: Cifar10
+    preprocessing = PadCropAndFlip(pad_size=40, output_size=32)
+    model: BinaryNet
+
+    epochs = 100
+    batch_size = 128
+
+    loss = "sparse_categorical_crossentropy"
+
+    metrics = ["acc"]
+
+    learning_rate = 5e-3
+
+    @property
+    def optimizer(self):
+        return tf.keras.optimizers.Adam(self.learning_rate)
+
+    def run(self):
+        with tf.device("/cpu:0"):
+            train_data = (
+                self.dataset.train_data()
+                .cache()
+                .map(partial(self.preprocessing, training=True))
+                .shuffle(10 * self.batch_size)
+                .repeat()
+                .batch(self.batch_size)
+            )
+            num_train_examples = self.dataset.num_train_examples
+            validation_data = (
+                self.dataset.validation_data()
+                .map(self.preprocessing)
+                .cache()
+                .repeat()
+                .batch(self.batch_size)
+            )
+            num_validation_examples = self.dataset.num_validation_examples
+            input_shape = train_data.output_shapes[0][1:]
+
+        model = self.model.build(input_shape=input_shape)
+
+        model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
+
+        lq.models.summary(model)
+
+        model.fit(
+            train_data,
+            epochs=self.epochs,
+            steps_per_epoch=math.ceil(num_train_examples / self.batch_size),
+            validation_data=validation_data,
+            validation_steps=math.ceil(num_validation_examples / self.batch_size),
+            callbacks=self.callbacks,
+        )
+
+
+if __name__ == "__main__":
+    cli()
