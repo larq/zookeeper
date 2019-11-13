b22a613caed087f4aac420c5c5a55f3abfb16fb4
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2019-11-13 22:19:00 +0000

    Make `Dataset` abstract and support a `MultiTFDSDataset`. (#63)
    * Make  an abstract base class.
    
    * Implement multi-dataset.
    
    * Rename `train_data` to `train`.

--- zookeeper/__init__.py
+++ zookeeper/__init__.py
@@ -1,8 +1,16 @@
 from zookeeper.component import Component
-from zookeeper.dataset import Dataset
+from zookeeper.dataset import Dataset, TFDSDataset
 from zookeeper.experiment import Experiment
 from zookeeper.task import Task
 from zookeeper.model import Model
 from zookeeper.preprocessing import Preprocessing
 
-__all__ = ["Component", "Dataset", "Experiment", "Model", "Preprocessing", "Task"]
+__all__ = [
+    "Component",
+    "Dataset",
+    "Experiment",
+    "Model",
+    "Preprocessing",
+    "Task",
+    "TFDSDataset",
+]
--- zookeeper/dataset.py
+++ zookeeper/dataset.py
@@ -1,8 +1,38 @@
-from typing import Optional
+from abc import abstractmethod
+from typing import Dict, Optional, Tuple
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+
 from zookeeper import Component
+
+
+class Dataset(Component):
+    """
+    An abstract base class to encapsulate a dataset. Concrete sub-classes must
+    implement the `train` method, and optionally the `validation` method.
+    """
+
+    @abstractmethod
+    def train(self, decoders=None) -> Tuple[tf.data.Dataset, int]:
+        """
+        Return a tuple of the training dataset and the number of training
+        examples in the dataset.
+        """
+
+        raise NotImplementedError
+
+    def validation(self, decoders=None) -> Tuple[tf.data.Dataset, int]:
+        """
+        Return a tuple of the validation dataset and the number of validation
+        examples in the dataset. By default, raises an error that no validation
+        data is provided.
+        """
+
+        raise ValueError(
+            f"Dataset '{self.__component_name__}' is not configured with validation "
+            "data."
+        )
 
 
 def base_splits(split):
@@ -16,7 +46,7 @@ def base_splits(split):
     return [split]
 
 
-class Dataset(Component):
+class TFDSDataset(Dataset):
     """
     A wrapper around a TensorFlowDatasets dataset.
     """
@@ -103,26 +133,75 @@ class Dataset(Component):
             as_dataset_kwargs={"shuffle_files": shuffle},
         )
 
-    def train_data(self, decoders=None) -> tf.data.Dataset:
-        return self.load(self.train_split, decoders=decoders, shuffle=True)
+    def train(self, decoders=None) -> Tuple[tf.data.Dataset, int]:
+        return (
+            self.load(self.train_split, decoders=decoders, shuffle=True),
+            self.num_examples(self.train_split),
+        )
 
-    @property
-    def num_train_examples(self) -> int:
-        return self.num_examples(self.train_split)
-
-    def validation_data(self, decoders=None) -> tf.data.Dataset:
+    def validation(self, decoders=None) -> Tuple[tf.data.Dataset, int]:
         if self.validation_split is None:
             raise ValueError(
                 f"Dataset {self.__class__.__name__} is not configured with a "
                 "validation split."
             )
-        return self.load(self.validation_split, decoders=decoders, shuffle=False)
+        return (
+            self.load(self.validation_split, decoders=decoders, shuffle=False),
+            self.num_examples(self.validation_split),
+        )
 
-    @property
-    def num_validation_examples(self) -> int:
-        if self.validation_split is None:
-            raise ValueError(
-                f"Dataset {self.__class__.__name__} is not configured with a "
-                "validation split."
+
+class MultiTFDSDataset(Dataset):
+    """
+    A wrapper around multiple TensorFlowDatasets datasets. This allows a model
+    to be trained on data that is combined from multiple datasets.
+    """
+
+    # A non-empty mapping from dataset names as keys to splits as values. The
+    # training data will be the concatenation of the datasets loaded from each
+    # (key, value) pair.
+    train_splits: Dict[str, str]
+
+    # As above, a mapping from dataset names as keys to splits as values. May be
+    # empty, indicating no validation data.
+    validation_splits: Dict[str, str] = {}
+
+    # The directory that the dataset is stored in.
+    data_dir: Optional[str] = None
+
+    def num_examples(self, splits) -> int:
+        """
+        Compute the total number of examples in the splits specified by the
+        dictionary `splits`.
+        """
+
+        return sum(
+            tfds.builder(name, data_dir=self.data_dir).info.splits[s].num_examples
+            for name, split in splits
+            for s in base_splits(split)
+        )
+
+    def load(self, splits, decoders, shuffle) -> tf.data.Dataset:
+        result = None
+        for name, split in self.train_splits.items():
+            dataset = tfds.load(
+                name=name,
+                split=split,
+                data_dir=self.data_dir,
+                decoders=decoders,
+                as_dataset_kwargs={"shuffle_files": shuffle},
             )
-        return self.num_examples(self.validation_split)
+            result = result.concatenate(dataset) if result is not None else dataset
+        return result
+
+    def train(self, decoders=None):
+        return (
+            self.load(self.train_splits, decoders=decoders, shuffle=True),
+            self.num_examples(self.train_splits),
+        )
+
+    def validation(self, decoders=None):
+        return (
+            self.load(self.validation_splits, decoders=decoders, shuffle=False),
+            self.num_examples(self.validation_splits),
+        )
