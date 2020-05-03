fd9072cc3b2c0cc9b44ed99a6077b842c1a8d39e
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-05-03 13:32:32 +0100

    :arrow_up: v1.0.1 (#143)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="zookeeper",
-    version="1.0.0",
+    version="1.0.1",
     author="Plumerai",
     author_email="lukas@plumerai.com, adamh@plumerai.com",
     description="A small library for managing deep learning models, hyper-parameters and datasets",
