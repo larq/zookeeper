69b7b14f1aa50944aa95027cea29780433e739a0
Jelmer Neeven <19648598+jneeven@users.noreply.github.com>
2020-11-30 15:05:38 +0100

    :arrow_up: 1.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="zookeeper",
-    version="1.0.4",
+    version="1.0.5",
     author="Plumerai",
     author_email="opensource@plumerai.com",
     description="A small library for managing deep learning models, hyper-parameters and datasets",
