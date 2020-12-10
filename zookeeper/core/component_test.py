f6ffad7e1e1a43c02f4162e8162532172d81036a
Jelmer Neeven <19648598+jneeven@users.noreply.github.com>
2020-12-10 18:19:05 +0100

    Update zookeeper/core/component_test.py
    Co-authored-by: Adam Hillier <7688302+AdamHillier@users.noreply.github.com>

--- zookeeper/core/component_test.py
+++ zookeeper/core/component_test.py
@@ -681,7 +681,6 @@ def test_component_configure_override():
             "The `__configure__` attribute of a @component class must be a method."
         ),
     ):
-
         @component
         class C:
             __configure__ = "test"
