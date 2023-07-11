553b621e67508b723780f3a80fbc7a8b1bc4bb82
Cedric Nugteren <web@cedricnugteren.nl>
2023-07-11 09:15:47 +0200

    Explicitly also pin protobuf version

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "click>=7.0",
         "tensorflow-datasets>=1.3.0,<v4.9.0",
         "typeguard>=2.5.1,<3.0.0",
+        "protobuf<3.20",
         "importlib-metadata ~= 2.0 ; python_version<'3.8'",
     ],
     extras_require={
