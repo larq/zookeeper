11b34947f84ffbafb5a80fa35c7b9ab2373ed79a
Cedric Nugteren <web@cedricnugteren.nl>
2023-07-11 10:32:27 +0200

    Disable TF 2.12 test

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "click>=7.0",
         "tensorflow-datasets>=1.3.0,<v4.9.0",
         "typeguard>=2.5.1,<3.0.0",
-        "protobuf<3.20",
+        "protobuf<3.20",  # for tensorflow-datasets
         "importlib-metadata ~= 2.0 ; python_version<'3.8'",
     ],
     extras_require={
